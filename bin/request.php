ec2b23849b2d4b46e28c97be6192a596d17c8b75
Rait Kapp <3442567+raigu@users.noreply.github.com>
2020-05-13 22:50:46 +0300

    Fix input was not read from STDIN

--- bin/request.php
+++ bin/request.php
@@ -8,7 +8,7 @@ Usage: php ./bin/request.php service client security_server_url
 
 Example: 
 $ echo "<prod:RR437 xmlns:prod="http://rr.x-road.eu/producer"><request><Isikukood>00000000000</Isikukood></request></prod:RR437>" | \
-  php ./bin/request.php EE/GOV/70008440/rr/RR437/v1 EE/COM/12213008/gathering https://security-server.client.com
+  php ./bin/request.php EE/GOV/70008440/rr/RR437/v1 EE/COM/00000000/gis https://security-server.client.com
 
 EOD;
 
@@ -25,14 +25,8 @@ $service = \Raigu\XRoad\Service::create(
     )
 );
 
-$response = $service->request(<<<EOD
-    <prod:testService xmlns:prod="http://test.x-road.fi/producer">
-        <request>
-            <responseBodySize>5</responseBodySize>
-            <responseAttachmentSize>0</responseAttachmentSize>
-         </request>
-    </prod:testService>
-EOD
+$response = $service->request(
+    file_get_contents("php://stdin")
 );
 
 echo $response;
